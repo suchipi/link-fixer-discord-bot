926567012a03af9870fd0fdfedc8ecd3384d1bfa
Ralph Drake <ralph@ralphdrake.com>
2023-12-20 13:16:56 -0500

    feat: add vxreddit.com, toggle sites with .env
    recovered from a system crash so a larger commit than usual.
    
    - add support for reddit.com and redd.it urls
    - use environment variables for all "fix" urls
    - don't fix a url if the environment variable is unset

--- src/replacements.ts
+++ src/replacements.ts
@@ -9,7 +9,6 @@ function logReplacement(fName: string, original: string, replacement: string) {
   console.log(`> [${fName}()] replacing ${original} -> ${replacement}`);
 }
 
-// NOTE: regexp must have the 'g' flag or else `matchAll` will throw
 function getUrls(content: string, regexp: RegExp): Array<string> {
   const urls: Array<string> = [];
   for (const matches of content.matchAll(regexp)) {
@@ -19,22 +18,16 @@ function getUrls(content: string, regexp: RegExp): Array<string> {
   return urls;
 }
 
-function fixYTShortsURL(content: string): string {
-  let c = content.replace(/(www\.)?(youtube.com\/shorts\/)/, "youtu.be/");
-  c = c.replace(/\?.*/, "");
-
-  logReplacement("fixYTShortsURL", content, c);
-
-  return c;
-}
+/*
+ * Link fixing functions
+ */
 
 function fixTwitterURL(content: string): string {
   let c = content.replace(
-    /\/\/(x|twitter).com\//,
-    `//${process.env.FXTWITTER_URL}/`,
+    /\/\/(x|twitter)\.com\//,
+    `//${process.env.TWITTER_FIX_URL}/`,
   );
-  // TODO: Configure a more robust way of stripping all the tracking crap off
-  //       of YouTube URLS (currently `(?|&)si=<trackingid>`)
+  // strip URL parameters from X links since they're only used for tracking
   c = c.replace(/\?.*/, "");
 
   logReplacement("fixTwitterURL", content, c);
@@ -42,8 +35,29 @@ function fixTwitterURL(content: string): string {
   return c;
 }
 
+function fixYTShortsURL(content: string): string {
+  let c = content.replace(
+    /(www\.)?(youtube\.com\/shorts\/)/,
+    `${process.env.YOUTUBE_FIX_URL}/`,
+  );
+
+  // strip URL parameters from YT links since they're only used for tracking
+  // TODO: make sure this doesn't negatively affect anything. I'm pretty sure
+  //       YT shorts don't support ?t= for timestamp or ?list= for playlists.
+  c = c.replace(/\?.*/, "");
+
+  logReplacement("fixYTShortsURL", content, c);
+
+  return c;
+}
+
 function fixInstagramURL(content: string): string {
-  let c = content.replace(/(www\.)?(instagram.com\/)/, "ddinstagram.com/");
+  let c = content.replace(
+    /(www\.)?(instagram\.com\/)/,
+    `${process.env.INSTAGRAM_FIX_URL}/`,
+  );
+
+  // TODO: Determine if we need to strip URL params from Instagram links
 
   logReplacement("fixInstagramURL", content, c);
 
@@ -51,12 +65,39 @@ function fixInstagramURL(content: string): string {
 }
 
 function fixTikTokURL(content: string): string {
-  let c = content.replace(/(tiktok.com\/)/, "vxtiktok.com/");
+  let c = content.replace(/(tiktok\.com\/)/, `${process.env.TIKTOK_FIX_URL}/`);
+
+  // TODO: Determine if we need to strip URL params from TikTok links
 
   logReplacement("fixTikTokURL", content, c);
 
   return c;
 }
+
+function fixRedditURL(content: string): string {
+  let c: string;
+
+  if (content.includes("reddit.com")) {
+    c = content.replace(
+      /((www|old|new|np)\.)?reddit\.com\//,
+      `${process.env.REDDIT_FIX_URL}/`,
+    );
+  } else if (content.includes("redd.it")) {
+    c = content.replace(/redd\.it\//, `${process.env.REDDIT_FIX_URL}/`);
+  } else {
+    throw new Error(`Could not determine a replacement for ${content}`);
+  }
+
+  // TODO: Determine if we need to strip URL params from TikTok links
+
+  logReplacement("fixRedditURL", content, c);
+
+  return c;
+}
+
+/*
+ * Replacements export
+ */
 
 export const replacements: {
   [identifier: string]: (content: string) => string | null;
@@ -64,9 +105,11 @@ export const replacements: {
   "x.com/": (content) => {
     const urls = getUrls(
       content,
+      // check for "status" here instead of the top level because usernames can
+      // vary
       /https?:\/\/x\.com\/(\w){1,15}\/status\/[^\s]+/g,
     );
-    if (urls.length > 0) {
+    if (process.env.TWITTER_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixTwitterURL(url)).join("\n");
     } else {
       return null;
@@ -75,9 +118,11 @@ export const replacements: {
   "twitter.com/": (content) => {
     const urls = getUrls(
       content,
+      // check for "status" here instead of the top level because usernames can
+      // vary
       /https?:\/\/twitter\.com\/(\w){1,15}\/status\/[^\s]+/g,
     );
-    if (urls.length > 0) {
+    if (process.env.TWITTER_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixTwitterURL(url)).join("\n");
     } else {
       return null;
@@ -85,15 +130,20 @@ export const replacements: {
   },
   "youtube.com/shorts/": (content) => {
     const urls = getUrls(content, /https?:\/\/(www\.)?youtube\.com\/[^\s]+/g);
-    if (urls.length > 0) {
+    if (process.env.YOUTUBE_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixYTShortsURL(url)).join("\n");
     } else {
       return null;
     }
   },
   "instagram.com/": (content) => {
-    const urls = getUrls(content, /https?:\/\/(www\.)?instagram\.com\/[^\s]+/g);
-    if (urls.length > 0) {
+    const urls = getUrls(
+      content,
+      // TODO: Add regex to make sure we only process reels and posts, not
+      //       profiles or searches etc.
+      /https?:\/\/(www\.)?instagram\.com\/[^\s]+/g,
+    );
+    if (process.env.INSTAGRAM_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixInstagramURL(url)).join("\n");
     } else {
       return null;
@@ -102,10 +152,37 @@ export const replacements: {
   "tiktok.com/": (content) => {
     const urls = getUrls(
       content,
+      // TODO: Add regex to make sure we only process reels and posts, not
+      //       profiles or searches etc.
       /https?:\/\/((www|vm)\.)?tiktok\.com\/[^\s]+/g,
     );
-    if (urls.length > 0) {
+    if (process.env.TIKTOK_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixTikTokURL(url)).join("\n");
+    } else {
+      return null;
+    }
+  },
+  "reddit.com/": (content) => {
+    const urls = getUrls(
+      content,
+      // TODO: Add regex to make sure we only process reels and posts, not
+      //       profiles, searches, subreddits, etc.
+      /https?:\/\/((www|old|new|np)\.)?reddit\.com\/[^\s]+/g,
+    );
+    if (process.env.REDDIT_FIX_URL && urls.length > 0) {
+      return urls.map((url) => fixRedditURL(url)).join("\n");
+    } else {
+      return null;
+    }
+  },
+  "redd.it/": (content) => {
+    const urls = getUrls(
+      content,
+      // TODO: Make sure we don't have to handle edge cases here
+      /https?:\/\/redd\.it\/[^\s]+/g,
+    );
+    if (process.env.REDDIT_FIX_URL && urls.length > 0) {
+      return urls.map((url) => fixRedditURL(url)).join("\n");
     } else {
       return null;
     }

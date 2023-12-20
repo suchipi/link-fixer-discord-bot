3969f9e48819ee6553a68f65340d9d85b06c1a5f
Ralph <ralph@podaboutli.st>
2023-12-20 11:36:20 -0500

    feat: add support for logging in dev
    defaults to off for simplified discord privacy

--- src/replacements.ts
+++ src/replacements.ts
@@ -1,6 +1,14 @@
 // import dotenv from "dotenv";
 // dotenv.config();
 
+function logReplacement(fName: string, original: string, replacement: string) {
+  if (!process.env.LINKFIX_LOGGING) {
+    return;
+  }
+
+  console.log(`> [${fName}()] replacing ${original} -> ${replacement}`);
+}
+
 // NOTE: regexp must have the 'g' flag or else `matchAll` will throw
 function getUrls(content: string, regexp: RegExp): Array<string> {
   const urls: Array<string> = [];
@@ -15,6 +23,8 @@ function fixYTShortsURL(content: string): string {
   let c = content.replace(/(www\.)?(youtube.com\/shorts\/)/, "youtu.be/");
   c = c.replace(/\?.*/, "");
 
+  logReplacement("fixYTShortsURL", content, c);
+
   return c;
 }
 
@@ -27,17 +37,23 @@ function fixTwitterURL(content: string): string {
   //       of YouTube URLS (currently `(?|&)si=<trackingid>`)
   c = c.replace(/\?.*/, "");
 
+  logReplacement("fixTwitterURL", content, c);
+
   return c;
 }
 
 function fixInstagramURL(content: string): string {
   let c = content.replace(/(www\.)?(instagram.com\/)/, "ddinstagram.com/");
 
+  logReplacement("fixInstagramURL", content, c);
+
   return c;
 }
 
 function fixTikTokURL(content: string): string {
   let c = content.replace(/((www|vm)\.)?(tiktok.com\/)/, "vxtiktok.com/");
+
+  logReplacement("fixTikTokURL", content, c);
 
   return c;
 }

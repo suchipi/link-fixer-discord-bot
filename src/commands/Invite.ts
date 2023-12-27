26df520f9a5398c3e990ce7f713a25a46ddbf5b8
Ralph Drake <rama.o.ralph@gmail.com>
2023-12-27 16:42:08 -0500

    Fix invite permissions (x2) (#18)
    * fix: add "Manage Messages" perm to invite url
    
    pr #15 added the feature to suppress message embeds. we need the "Manage
    Messages" permission in order to successfully do so.
    
    * fix: new invite url in /invite
    
    also add the check-merge-conflict pre-commit hook just in case...

--- src/commands/Invite.ts
+++ src/commands/Invite.ts
@@ -8,7 +8,7 @@ export const InviteCommand: CustomCommand = {
   execute: async (interaction: CommandInteraction) => {
     await interaction.reply({
       content:
-        "You can use this link to add LinkFix to your server:\nhttps://discord.com/oauth2/authorize?client_id=385950397493280805&scope=bot&permissions=274878286912",
+        "You can use this link to add LinkFix to your server:\nhttps://discord.com/oauth2/authorize?client_id=385950397493280805&scope=bot%20applications.commands&permissions=274877934592",
       ephemeral: true,
     });
   },

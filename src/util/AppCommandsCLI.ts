869dc90ec185c7b23cf70b356adb5e186bee50da
Ralph Drake <ralph@podaboutli.st>
2024-01-09 15:20:08 -0500

    v1.7.1

--- src/util/AppCommandsCLI.ts
+++ src/util/AppCommandsCLI.ts
@@ -221,7 +221,7 @@ const deleteCommands: (args: {
     .description(
       "CLI tool for updating Discord application commands\n\nNOTE: When running via NPM package script, pass '--' before any arguments.\nExample: npm run appcmd-cli -- --help",
     )
-    .version("1.7.0");
+    .version("1.7.1");
 
   program
     .command("sync")

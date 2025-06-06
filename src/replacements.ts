ae4fe2893771622008d216a0db5e2d98938a4ca3
Lily Skye <git@suchipi.tv>
2025-06-06 17:22:34 -0600

    the people have spoken

--- src/replacements.ts
+++ src/replacements.ts
@@ -18,7 +18,7 @@ export const replacements: {
     );
     if (urls.length > 0) {
       return urls
-        .map((url) => url.replace("//x.com/", "//vxtwitter.com/"))
+        .map((url) => url.replace("//x.com/", "//girlcockx.com/"))
         .join("\n");
     } else {
       return null;
@@ -31,7 +31,7 @@ export const replacements: {
     );
     if (urls.length > 0) {
       return urls
-        .map((url) => url.replace("//twitter.com/", "//vxtwitter.com/"))
+        .map((url) => url.replace("//twitter.com/", "//girlcockx.com/"))
         .join("\n");
     } else {
       return null;
